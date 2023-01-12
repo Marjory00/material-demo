3c7d62381b3562a149849047950895464a626db9
Marjory Marquez <samaterra@hotmail.com>
2023-01-12 17:05:11 -0500

    typography link

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
+const MaterialComponents = [
+  MatButtonModule
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+   
+    MaterialComponents,
+    
+  ]
+})
+export class MaterialModule { }
